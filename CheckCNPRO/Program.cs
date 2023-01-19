ff1c7bd65b0d61d7d9743817c7d2feeab7b36dfa
Andrei Rachita <andrei@rachita.net>
2023-01-19 18:15:29 +0200

    Add project files.

---
+++ CheckCNPRO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("CNP Checker for Romanian citizens");
+        Console.Write("Enter CNP: ");
+        string cnp = Console.ReadLine();
+
+        if (cnp.Length != 13)
+        {
+            Console.WriteLine("Invalid CNP. Must be 13 digits.");
+            return;
+        }
+
+        int[] weights = { 2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9 };
+        int sum = 0;
+
+        for (int i = 0; i < 12; i++)
+        {
+            sum += weights[i] * int.Parse(cnp[i].ToString());
+        }
+
+        int controlDigit = sum % 11;
+        if (controlDigit == 10) controlDigit = 1;
+        int lastDigit = int.Parse(cnp[12].ToString());
+
+        if (controlDigit == lastDigit)
+        {
+            Console.WriteLine("Valid CNP.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid CNP.");
+        }
+    }
+}
